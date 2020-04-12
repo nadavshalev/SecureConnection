516da2aaf669c9daf8d9bc7eda6d89dfa508cc52
nadav shalev <nadav.shalev@outlook.com>
2020-04-12 15:06:19 +0300

    server connect between client first try

--- client.py
+++ client.py
@@ -1,14 +1,35 @@
 from Connection.ConnSecure import ConnSecureClient
 from Connection.ConnSocket import ConnSocketClient
+import json
+import threading
+
+
+def receive_msg(sc):
+    print('inside!!!!!')
+    while True:
+        data = sc.receive()
+        if not data:
+            exit(0)
+        print(data)
+
 
 f = open('./Connection/log_conn_client.txt', 'a')
 conn_socket = ConnSocketClient('127.0.0.1', 65432, f)
 conn_secure = ConnSecureClient(conn_socket, f)
 conn_secure.connect()
+
+threading.Thread(target=receive_msg, args=(conn_secure,)).start()
+
+addr = input('Write Address: ')
 while True:
-    msg = input('Write Message Q[quit]: ')
-    if msg == 'Q':
+    msg_str = input('Write Message Q[quit]: ')
+    if msg_str == 'Q':
         conn_secure.disconnect()
-        break
-    conn_secure.send(msg)
-    print(conn_secure.receive())
+        exit(0)
+
+    msg = {
+        "to": addr,
+        "msg": msg_str
+    }
+    json_msg = json.dumps(msg)
+    conn_secure.send(json_msg)
--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from Connection.ConnSecure import ConnSecureServer
 from Connection.ConnSocket import ConnSocketServer
 import socket
 import threading
+import json
+
 
 class ThreadedServer:
 	def __init__(self, host, port):
@@ -10,6 +12,7 @@ class ThreadedServer:
 		self.port = port
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.bind((self.host, self.port))
+		self.conn_dict = dict()
 
 	def listen(self):
 		self.sock.listen(5)
@@ -23,6 +26,8 @@ class ThreadedServer:
 		obj_sock = ConnSocketServer(conn, f, addr)
 		obj_secure = ConnSecureServer(obj_sock, f)
 
+		self.conn_dict[repr(addr)] = obj_secure
+
 		if not obj_secure.connect():
 			print(repr(addr) + ': could not connect')
 			return
@@ -30,15 +35,18 @@ class ThreadedServer:
 		try:
 			# receive and send until closed
 			while True:
-				msg = obj_secure.receive()
-				if not msg:
+				msg_str = obj_secure.receive()
+				if not msg_str:
 					print(repr(addr) + ': connection closed')
 					break
-				print(repr(addr) + ': ' + msg)
-				new_msg = 'server response... ' + msg
-				obj_secure.send(new_msg)
-		except:
-			print(repr(addr) + ': connection broke')
+
+				msg = json.loads(msg_str)
+				print(f'from: {repr(addr)}\t to: {msg["to"]}\t data: {msg["msg"]}')
+
+				self.conn_dict[msg["to"]].send(msg["msg"])
+				# obj_secure.send(msg["msg"])
+		except Exception as e:
+			print(repr(addr) + ': connection broke - ' + repr(e))
 			return
 
 
